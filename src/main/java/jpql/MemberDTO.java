fb7f11ee054ee7d774493082c98c92dc74dda53b
woojin126 <woojin126@naver.com>
2021-06-26 17:18:09 +0900

    MemberDTO  프로젝션 여러값 조회 방법중 하나.

--- src/main/java/jpql/MemberDTO.java
+++ src/main/java/jpql/MemberDTO.java
@@ -1,2 +1,16 @@
-package jpql;public class MemberDTO {
+package jpql;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class MemberDTO {
+
+    private String username;
+    private int age;
+
 }
