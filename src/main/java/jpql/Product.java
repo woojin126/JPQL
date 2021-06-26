178ceb196ac50f01e61402266359063baa7f742c
woojin126 <woojin126@naver.com>
2021-06-26 17:17:09 +0900

    product entity

--- src/main/java/jpql/Product.java
+++ src/main/java/jpql/Product.java
@@ -1,2 +1,21 @@
-package jpql;public class Product {
+package jpql;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private int price;
+    private int stockAmount;
 }
