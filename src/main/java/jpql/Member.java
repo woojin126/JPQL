82f38a10ee51b2324cdc91b99af8762e09fe8092
woojin126 <woojin126@naver.com>
2021-06-26 17:16:00 +0900

    MemberEntity

--- src/main/java/jpql/Member.java
+++ src/main/java/jpql/Member.java
@@ -1,2 +1,31 @@
-package jpql;public class Member {
+package jpql;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
