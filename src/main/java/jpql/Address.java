e06095fff10f32fee4d88611ef8abfc5023ca146
woojin126 <woojin126@naver.com>
2021-06-26 17:15:15 +0900

    @Embedded Address.class

---
+++ src/main/java/jpql/Address.java
@@ -0,0 +1,17 @@
+package jpql;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Embeddable;
+
+@Getter
+@Setter
+@Embeddable
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+}
