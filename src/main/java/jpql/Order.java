f192b86aa16dd332a575fb4077b9f26b47efdf41
woojin126 <woojin126@naver.com>
2021-06-26 17:16:42 +0900

    Order entity

--- src/main/java/jpql/Order.java
+++ src/main/java/jpql/Order.java
@@ -1,2 +1,24 @@
-package jpql;public class Order {
+package jpql;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Setter
+@Getter
+@Table(name="ORDERS")
+public class Order {
+
+    @Id @GeneratedValue
+    private Long id;
+    private int orderAmount;
+
+    @Embedded
+    private Address address;
+
+    @ManyToOne
+    @JoinColumn(name = "PRODUCT_ID")
+    private Product product;
 }
