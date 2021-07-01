6179b39ad3580ab6047eb548cab35865bdcceb7e
woojin126 <woojin126@naver.com>
2021-07-01 11:05:41 +0900

    @NameQuery 추가

--- src/main/java/jpql/Member.java
+++ src/main/java/jpql/Member.java
@@ -8,6 +8,10 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
+@NamedQuery( //관례가 엔티티명.이름
+        name = "Member.findByUsername",
+        query = "select m from Member m where m.username = :username"
+)
 public class Member {
 
     @Id @GeneratedValue
@@ -22,7 +26,7 @@ public class Member {
     @Enumerated(EnumType.STRING)
     private MemberType type;
 
-    public void changeTeam(Team team){
+    public void setTeam(Team team){
         this.team = team;
         team.getMembers().add(this);
     }
