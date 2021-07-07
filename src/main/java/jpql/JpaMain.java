c2eabd23076ddecdcb5463674833ac37ccf28334
woojin126 <woojin126@naver.com>
2021-07-07 19:22:47 +0900

    join / join fetch / 일대다 관계의 페이징 위험성

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -87,6 +87,69 @@ public class JpaMain {
 */
             em.flush();
             em.clear();
+
+         /*   List<Member> resultList = em.createQuery("select m from Member m join m.team t",Member.class).getResultList();
+            for (Member member : resultList) {
+                System.out.println("member = " + member);
+                System.out.println("member.getTeam() = " + member.getTeam());
+
+            }*/
+
+
+           /* List<Team> result = em.createQuery("select t from Team t join fetch t.members",Team.class)
+                    .setFirstResult(0)
+                    .setMaxResults(1)
+                    .getResultList();
+            for (Team team : result) {
+                System.out.println("team = " + team.getName());
+
+                for(Member member : team.getMembers()){
+                    System.out.println("member = " + member);
+                }
+            }*/
+
+        /*    List<Member> resultList = em.createQuery("select m from Member m join fetch m.team", Member.class)
+                    .setFirstResult(0)
+                    .setMaxResults(1)
+                    .getResultList();
+
+            for (Member member : resultList) {
+                System.out.println("member = " + member);
+            }*/
+
+        /*    List<Team> resultList = em.createQuery("select t from Team t join t.members",Team.class)
+                    .setFirstResult(0)
+                    .setMaxResults(2)
+                    .getResultList();
+
+            for (Team team : resultList) {
+                System.out.println("team = " + team.getName());
+                for (Member member : team.getMembers()){
+                    System.out.println("member = " + member);
+                }
+            }*/
+            List<Team> t = em.createQuery("select t from Team t", Team.class).getResultList();
+
+            System.out.println("t.size() = " + t.size());
+            for (Team team : t) {
+                System.out.println("team === " + team.getName());
+
+                for (Member member : team.getMembers()){
+                    System.out.println("---------->member = " + member);
+                }
+            }
+
+
+            List<Team> tt = em.createQuery("select t from Team t join t.members m", Team.class).getResultList();
+
+            System.out.println("tt.size() = " + tt.size());
+            for (Team team : tt) {
+                System.out.println("team === " + team.getName());
+
+                for (Member member : team.getMembers()){
+                    System.out.println("---------->member = " + member);
+                }
+            }
             /**
              * 반환 타입이 명확하면 TypeQuery
              * 반환 타입이 불명확하면 Query
@@ -583,26 +646,33 @@ public class JpaMain {
              */
             //일대다 중심 쿼리인 아래쿼리로는 페이징이 매우 위험.
             //WARN org.hibernate.hql.internal.ast.QueryTranslatorImpl - HHH000104: firstResult/maxResults specified with collection fetch; applying in memory!
-            String query = "select t from Team t join fetch t.members";
+    /*        String query = "select t from Team t join fetch t.members m";
 
             //대안1
             //맴버를 중심으로 페이징 시작 회원 -> 팀은 다대일 (이러면 페이징 위험 x)
-            String query1 = "select m From Member m join fetch m.team t";
+            String query1 = "select m From Member m join fetch m.team";
 
             //대안2
             //XML에 BATCH 사이즈 설정 이러면 N+1로안나가고 테이블 수만큼 맞춰서나가게 할 수있다.
             //Team 엔티티에서 BatchSize 설정
             //LAZY로딩이고, 조회가 여러번되기때문에 성능은 최악
             //팀을한 10개 조회하면,, 연관된 맴버쿼리가 추가로 10번이 더나감...
-/*            String query2 = "select t From Team t";
+           String query2 = "select t From Team t";
 
             List<Team> result = em.createQuery(query2,Team.class)
                     .setFirstResult(0)
-                    .setMaxResults(8)
+                    .setMaxResults(1)
                     .getResultList();
 
-            System.out.println("result.size() = " + result.size());*/
+            System.out.println("result.size() = " + result.size());
+            for (Team team : result) {
+                System.out.println("team = " + team.getName());
 
+                for (Member member  : team.getMembers()){
+                    System.out.println("------------> member = " + member);
+                }
+            }
+*/
           /*  for (Member member : result) {
                 System.out.println("------------> member.getUsername() = " + member.getTeam());
             }*/
@@ -702,7 +772,7 @@ public class JpaMain {
             //모든회원의 나이를 20살로 바꿔보자, 아래 update 벌크연산 처리전에 FLUSH 자동호출
             //에는 영속성 컨텍스트랑 상관없이 그냥 db에 값을넣어버림;;
             //영속 컨텍스트에는 age = 20 이 반영 x
-            int resultCount = em.createQuery("update Member m set m.age =20")
+         /*   int resultCount = em.createQuery("update Member m set m.age =20")
                     .executeUpdate();
 
             System.out.println("resultCount = " + resultCount);
@@ -714,7 +784,7 @@ public class JpaMain {
 
             System.out.println("findMember.getAge() = " + findMember.getAge());
 
-
+*/
             /**
              * 벌크 연산 주의
              * 1.벌크 연산은 영속성 컨텍스트를 무시하고 데이터베이스에 직접 쿼리 날려버림(flush() 발생)
